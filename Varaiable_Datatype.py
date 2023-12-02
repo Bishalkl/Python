be2541c2142c044c0989195464a1fb8fe090907a
Bishalkl <bishal123koirala@gmail.com>
2023-12-02 09:00:31 +0545

    Making new repo for python

---
+++ Varaiable_Datatype.py
@@ -0,0 +1,12 @@
+
+######## Data types and variables #################
+age = 12 # int
+height = 5.6 # float or decimial value 
+name = "Bishal koirala" #String
+are_you_boy = True #Either true or false
+numbers = [1,2,3,4,5] #List are ordered, mutable collection
+coordinate = (4,5) #Tuples are ordered, immutable collection
+person = {'name': 'Bishal', 'age': 30} #These are collection of key-value pairs
+
+
+print(age, height, name, are_you_boy, numbers, coordinate, person)

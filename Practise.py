ed8146c717f4a657d7d0de5403cb3fb3a6934379
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2023-12-05 18:23:44 +0545

    update practise

---
+++ Practise.py
@@ -0,0 +1,17 @@
+#Arithimetic Operators. 
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("The sum is ", num1+num2)
+print("The difference is ", num1-num2)
+print("The product is ", num1*num2)
+print("The quotient is ", num1 // num2)
+
+#Comparison Operators.
+age = int(input("Enter your age: "))
+if(age >= 18):
+    print("You are eligible to vote !!!")
+else:
+    print("You are not eligible to vote !!!")
+
+# 

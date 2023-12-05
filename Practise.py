6e7a6a9e6092cce2969bc0b97b3684374c88ceaf
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2023-12-05 19:17:59 +0545

    update

--- Practise.py
+++ Practise.py
@@ -32,3 +32,53 @@ list2 = list1
 
 result = list1 is list2
 print(result) 
+
+#If-else Statement
+c = int(input("Enter the number: "))
+if(c >=1):
+    print("It is positive number.")
+elif(c < 0):
+    print("It is negative number.")
+else:
+    print("It is zero")
+
+#For Loop
+d = int(input("Enter the number: "))
+for number in range(d-1):
+    print(str(d)+"*"+str(number)+" = ",d * number)
+
+#While Loop
+import random
+correct_number = random.randint(1,10)
+guess_number = int(input("Enter the number between 1 to 10: "))
+
+while True:
+    if(guess_number == correct_number):
+        print("You have guessed correct number !!!")
+        break
+    elif(guess_number > correct_number):
+        print("Too high, Try again.")
+        break
+    else:
+        print("Too low, Try again.")
+        break
+
+#Break and Continue:
+count = 1
+for number in range(2,30,2):
+    print(number)
+    count +=1
+
+    if count == 10:
+        break
+
+#Exception Handling
+f = int(input("Enter the first number: "))
+g = int(input("Enter the second number: "))
+
+try: 
+    result = f / g
+    print("The result is ",result)
+except:
+    print("Sorry we can't divide by number zero, Try again !!!")
+

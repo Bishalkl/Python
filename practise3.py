9c6098d4e07034a4e1c5da0cdf03798ed0498eaa
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2023-12-16 11:57:39 +0545

    done
    done

--- practise3.py
+++ practise3.py
@@ -1,4 +1,6 @@
-
-for num in range(1,5):
-    square = num**2
-    print(square)
+x = int(input("Enter the value: "))
+if(x>0 and x<10):
+    print("Yes, it's range between 0 to 10.")
+else:
+    print("No, it's not range between 0 to 10.")
+    

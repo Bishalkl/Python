d8c6c5f761dfdc4168caaaa6edba2385275af22c
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2023-12-18 16:42:28 +0545

    done

--- fucprac.py
+++ fucprac.py
@@ -3,3 +3,21 @@ def sum(a, b):
     return a * b
 
 print(sum(1,2))
+
+
+#Practise two
+def facto(input):
+    fac = 1
+    for i in range(1,input+1):
+        fac *= i
+    return fac
+
+print(f"The factorial of given number is {facto(10)}")
+
+
+#Practise three
+def checkNum(number):
+    
+
+    
+

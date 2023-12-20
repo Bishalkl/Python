a79cfb48e52f48efc7dcce98da20b2442f26f3bc
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2023-12-20 17:15:07 +0545

    done

---
+++ datastructure.py
@@ -0,0 +1,30 @@
+#Built-in Data structures
+
+#creating in list
+list1 = [1,2,3,"bishal","Animation",2, 4, 5, 6,]
+print(list)
+
+#add = append(), extend(), and insert()
+
+b = [1,2,3,4,5,6]
+b.append("Bishal")
+print(b)
+
+
+#delete = del, pop(), remove()
+
+del list1[0] # it is use to delete the data targeting the index of l
+print(list1)
+
+a = list1.pop(5) # it is use to pop the element and print it out in targeting the index of list
+print(a)
+
+list2 = [1,2,3,4,5,6,7,"komal", "Animation"] # it remove the data targeting the exact the value 
+list2.remove("komal")
+print(list2)
+
+
+
+
+
+

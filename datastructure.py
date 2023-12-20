4b30ddf0d0ba5dbf8651e153be240055b502044f
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2023-12-20 17:42:41 +0545

    done

--- datastructure.py
+++ datastructure.py
@@ -7,8 +7,18 @@ print(list)
 #add = append(), extend(), and insert()
 
 b = [1,2,3,4,5,6]
-b.append("Bishal")
+b.append("Bishal") #append is use to data in list and will be store in last index
 print(b)
+
+c = [1,2,3,5,6,7,3,5,]
+c.extend(("ANimation",8)) #it i will split if we put string single and also put double bracket to store it multiple data 
+print(c)
+
+d = [1,2,3,4,5]
+d.insert(2,"bishal") #need index and also value where value should be put and will capture a other index value and push it next beyond it.
+print(d)
+
+
 
 
 #delete = del, pop(), remove()
@@ -23,6 +33,10 @@ list2 = [1,2,3,4,5,6,7,"komal", "Animation"] # it remove the data targeting the 
 list2.remove("komal")
 print(list2)
 
+
+#sort: 
+e = [1,2,3,12,9,17,45,25]
+print(sorted(a))
 
 
 

82d868855530a6c045942dcd539fa25aed60dcc7
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2023-12-23 08:15:57 +0545

    done

--- prac_ds.py
+++ prac_ds.py
@@ -68,6 +68,6 @@ print(intersection_number, difference_number)
 fav_color = ("Red", "Blue", "Green", "purple")
 print(fav_color)
 #tuple = tuple are also the data structure but they are immutable.
-
-
-
+fav_color [1] = "yellow"
+print(fav_color)
+#tuple does not support item assignment

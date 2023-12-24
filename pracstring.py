8a0e8774e8fdb3e8089ac98c53ae0748d967ed43
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2023-12-24 13:57:49 +0545

    done

--- pracstring.py
+++ pracstring.py
@@ -87,6 +87,14 @@ message1 = str(input("Enter a sentence: "))
 print(message1.title())
 
 #practise ten
+def checkAnagram(str1,str2):
+    str1 = str1.replace(" ","").lower()
+    str2 = str2.replace(" ","").lower()
+    return sorted(str1) == sorted(str2)
+
+print(checkAnagram("listen","enstil"))
+
+
 
 
     

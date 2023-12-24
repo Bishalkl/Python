47140242dcbe155283dc2de937c35b10b42b5667
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2023-12-24 12:40:06 +0545

    done

--- pracstring.py
+++ pracstring.py
@@ -3,6 +3,22 @@
 def reverseString(string): #creating the fuction
     return string[::-1]  #returning the value after reversing the string 
 
-print(reverseString("Bishal"))
+print(reverseString("Bishal koirala"))
 
-#
+#practise two
+def countVowels(string): 
+    VowelsList = ['a','e','i','o','u'] 
+    count = 0 
+    string = string.lower() 
+    for i in string:
+        for x in VowelsList:
+            if i == x:
+                count += 1
+
+    return count  
+
+print(countVowels("Bishal"))  
+                
+
+
+

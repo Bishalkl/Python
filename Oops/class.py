a0dc291b551e3e019d061dabf684513b182c9327
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2024-01-02 19:53:03 +0545

    done

--- Oops/class.py
+++ Oops/class.py
@@ -10,16 +10,17 @@ demoobject.sum()
 
 class Employee:
 
-    def putdata(self):
-        self.id = int(input("enter eimployee id"))
-        self.name = input("enter the employee name ")
+    def putdata(self): #Method one which is for get a information.
+        self.id = int(input("enter Employee id: "))
+        self.name = input("enter the Employee name: ")
+        self.salary = int((input("enter Employee salary: ")))
 
-        self.salary = input(input("enter employee salary"))
-
-    def display(self):
-        print(f"Id: {self.id}")
-        print(f"Name:  {self.name}")
-        print(f"Total salary : {self.salary}")
+    def display(self): #Method two which is for print detail.
+        print(f"Id: {self.id}.")
+        print(f"Name: {self.name}.")
+        print(f"Total salary : Rs.{self.salary}/only.")
     
 
-
+a = Employee() #creating the object
+a.putdata() #calling object for input 
+a.display() #calling object for display 

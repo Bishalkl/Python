17d88ab2c3309bc5a646fea978bef9d8301fc370
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2024-01-03 18:57:56 +0545

    done

--- Oops/revone.py
+++ Oops/revone.py
@@ -1,3 +1,6 @@
+
+####Q1
+
 #creating a class 
 class Rectangle:
 
@@ -17,8 +20,25 @@ my_rectangle = Rectangle(12.3,34.4)
 print(f"The area of rectangle is {my_rectangle.area()}.")
 
 
+###Q2
 
+#creating the class 
+class Person:
 
+    #initializes the attributes:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    #creating the method for passing a parameter
+    def detail(self):
+        return f"Name is {self.name} and Age is {self.age}."
+
+#creating instance 
+personOne = Person("Alice",20)
+persontwo = Person("Bob", 30)
+print(personOne.detail())
+print(persontwo.detail())
         
         
         

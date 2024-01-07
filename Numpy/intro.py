4ce652a50b8c30f531e69620484b6f83678550cb
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2024-01-07 15:37:39 +0545

    done

--- Numpy/intro.py
+++ Numpy/intro.py
@@ -6,5 +6,9 @@ my_list = [1,2,3] #first creating a list
 my_arrays = np.array(my_list) # And converting into numpy arrays and also storing into variable my_arrays.
 print(my_arrays)
 
-#
+#Creating  a Tuple 
+my_tuple = (4,5,6) #first creating a tuple 
+my_array_from_tuple = np.array(my_tuple)
+print(my_array_from_tuple)
+
 

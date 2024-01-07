7da8a3027ec0b13df2b0b51177f4d598f668928d
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2024-01-07 15:51:10 +0545

    done

--- Numpy/intro.py
+++ Numpy/intro.py
@@ -11,4 +11,22 @@ my_tuple = (4,5,6) #first creating a tuple
 my_array_from_tuple = np.array(my_tuple)
 print(my_array_from_tuple)
 
+#Creating Zeros and Ones Arrays:
+zero_array = np.zeros(4) # it will make 4 zero matrix
+one_array = np.ones((3,4)) # it will make 3 dimension and 4 ones' value in each arrays 
+print(one_array)
+print(zero_array)
+
+#Creating Range Arrays:
+range_array = np.arange(0,10,) #it will work like a range in loop
+print(range_array)
+
+#creating Linspace Arrays
+linspace_array = np.linspace(1,20,5)
+print(linspace_array)
+
+#Shape and Dimension
+new_array = np.array([1,2,3,4,5])
+print(new_array.shape)
+print(new_array.ndim)  
 

e6431cfbb723d3934728d3fcfe7391470f3f1a04
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2024-01-07 15:35:57 +0545

    Done

--- Numpy/intro.py
+++ Numpy/intro.py
@@ -1,2 +1,10 @@
 #importing numpy module
 import numpy as np
+
+#Creating rom a list 
+my_list = [1,2,3] #first creating a list
+my_arrays = np.array(my_list) # And converting into numpy arrays and also storing into variable my_arrays.
+print(my_arrays)
+
+#
+

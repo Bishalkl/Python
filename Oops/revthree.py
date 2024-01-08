f1412ff705dd8a6df5c09d7199957c9b90fc53f0
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2024-01-08 19:40:17 +0545

    done

--- Oops/revthree.py
+++ Oops/revthree.py
@@ -25,3 +25,49 @@ class Employee(Person):
 #creating instance
 Person_detail = Employee("Bishal koirala", 12, 120)
 Person_detail.display_detail()
+
+
+#Multiple Inheritance
+
+#Q1
+
+#Class Database
+class Database:
+    
+    def save():
+        print("Data saved!!!")
+
+
+#class Logger
+class Logger:
+    
+    def Log():
+        print("Log a message")
+
+#class Application
+class Application:
+    pass
+
+#class Webapp
+class Webapp(Database,Logger):
+
+    def __init__(self,data,message):
+        self.data = data
+        self.message = message
+
+    def display(self):
+
+        print(f"Data is \'{self.data}\' and message is \"{self.message}\".")
+        Database.save()
+        Logger.Log()
+
+#create a instance 
+first_Webapp = Webapp("dfdfdfjkdisdf+63dfdf45df2","I liked it.")
+first_Webapp.display()
+
+
+
+#Multileve Inheritance
+
+#class
+class 

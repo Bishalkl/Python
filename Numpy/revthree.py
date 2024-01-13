e3f190f632ea7770f12c17cb595d751528c5667d
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2024-01-13 07:45:55 +0545

    done

---
+++ Numpy/revthree.py
@@ -0,0 +1,28 @@
+#Arithmetic Function
+
+import numpy as np 
+# Q1
+var = np.array([1,8,5,3,7])
+print(np.mean(var))
+
+#q2
+var1 = np.array([[2,1,3],[9,5,6]])
+print(np.add(var1[0],var1[1]))
+
+#Square Root and Trigonometric Function:
+# angles = np.array([0,np.pi/2,np.p1])
+# tangent_value = np.tan(angles) #tanget mean tan 
+# print(tangent_value)
+
+var3 = np.array([4,16,25])
+print(np.sqrt(var3))
+
+
+#Cumulative Sum:
+nums = np.array([1,2,3,4,5])
+print(np.cumsum(nums))
+
+
+
+
+

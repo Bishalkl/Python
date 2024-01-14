17533267c4c5b0b8a4bd4e1538cca4f890f0fe86
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2024-01-14 15:26:37 +0545

    done

--- Oops/revfive.py
+++ Oops/revfive.py
@@ -47,3 +47,36 @@ print(f"The vlaue is total vechile: .")
 show_vehicle_info(total_Vechile)
 
 
+#Question 2
+
+#class 
+class ComplexNumber:
+
+    def __init__(self,real,img):
+        self.real = real
+        self.img = img
+    
+    #overload
+    def __add__(self,other):
+        add_real = self.real + other.real
+        add_img = self.img + other.img
+        return ComplexNumber(add_real, add_img)
+
+    #overload   
+    def __mul__(self,other):
+        multi_real = self.real * other.real - self.img * other.img
+        multi_img = self.img * other.img + self.real * other.real
+        return ComplexNumber(multi_real, multi_img)
+    
+    def __str__(self):
+        return f"{self.real} + {self.img}"
+#instance 
+a1 = ComplexNumber(12,1)
+a2 = ComplexNumber(12,1)
+
+result_add = a1 + a2
+result_multi = a1 * a2   
+
+print(f"The result_add = {result_add}.")
+print(f"The result_multi = {result_multi}")
+

4fc4b9c58f38a0eb0867f238f135890dd5bdb962
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2024-01-18 10:45:03 +0545

    done

---
+++ Numpy/revfive.py
@@ -0,0 +1,26 @@
+import numpy as np 
+
+#q1
+a = np.array([[1,2,3],[4,7,8]])
+for i in a:
+    for j in i:
+        print(j)
+
+#q2
+b = np.array([[[1,3,4,7],[8,10,6,2]],[[1,2,3,9],[8,4,2,10]]])
+for i in np.nditer(b):
+    if(i % 2 == 0):
+        print(i)
+
+#q3
+c = np.arange(1,21)
+d = c.reshape(4,5)
+for i, j in np.ndenumerate(d):
+    p = j*2
+    print(i,p)
+
+#q4
+e = np.array([1,2,3,4,5])
+f = np.copy(e)
+e = np.array([i*2 for i in e]) 
+print(f,e)
